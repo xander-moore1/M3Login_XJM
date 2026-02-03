408f0d798aecd3009b96a14dcdec3c3370d1745d
XJM <xander_moore1@mymail.eku.edu>
2026-02-03 12:04:06 -0500

    Added basic checks for username and password to app

--- M3Login_XJM/ViewController.swift
+++ M3Login_XJM/ViewController.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!;
+    @IBOutlet weak var passwordTextField: UITextField!;
+    @IBOutlet weak var resultLabel: UILabel!;
+    
+    @IBAction func loginButton(_ sender: Any) {
+        let user = usernameTextField.text!;
+        let pwd = passwordTextField.text!;
+        
+        resultLabel.text = (user == "XJM" && pwd == "1234") ? "Login successful" : (user.isEmpty || pwd.isEmpty) ? "Enter username & password" : "User not recognized";
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view.
     }
 
